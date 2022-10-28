d8fec2bcb0b9233681fd5a7999cea256346e22be
Chayan Chandan das <chayanchandandas@gmail.com>
2022-10-28 21:32:46 +0600

    push js file and link

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # To-Do-List
+[click for live site](https://chayan999.github.io/To-Do-List/)
---
+++ script.js
@@ -0,0 +1,70 @@
+const fromEl = document.querySelector('.form');
+const inputEl = document.querySelector('.input');
+const ulEl = document.querySelector('.list')
+
+//----gat local storage
+
+let list = JSON.parse(localStorage.getItem('list'))
+list.forEach(task => {
+    toDoList(task)
+})
+
+fromEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+    toDoList();
+});
+
+function toDoList(task) {
+    let newTask = inputEl.value;
+
+    //---- set for local strage
+    if (task) {
+        newTask = task.name
+    }
+
+    //---- set for local strage
+    let liEl = document.createElement('li');
+    if (task && task.checked) {
+        liEl.classList.add('checked')
+    }
+    liEl.innerText = newTask;
+    ulEl.appendChild(liEl);
+    inputEl.value = '';
+
+    //---creat check button
+    const checkBtn = document.createElement('div');
+    checkBtn.innerHTML = `<i class="fas fa-check-square"></i>`
+    liEl.appendChild(checkBtn);
+
+
+    //---- creat trash button
+    const trashBtn = document.createElement('div');
+    trashBtn.innerHTML = `<i class="fa-sharp fa-solid fa-trash"></i>`;
+    liEl.appendChild(trashBtn);
+
+    //---- torgging checked button
+    checkBtn.addEventListener('click', () => {
+        liEl.classList.toggle('checked')
+        setStorage()
+    });
+
+    //---- remove li element by trash button
+    trashBtn.addEventListener('click', () => {
+        liEl.remove()
+        setStorage()
+    })
+    setStorage()
+};
+
+//---- function for Local Stograge
+function setStorage() {
+    let liEls = document.querySelectorAll('li')
+    let list = []
+    liEls.forEach((liEl) => {
+        list.push({
+            name: liEl.innerText,
+            checked: liEl.classList.contains('checked')
+        })
+    });
+    localStorage.setItem('list', JSON.stringify(list))
+}
